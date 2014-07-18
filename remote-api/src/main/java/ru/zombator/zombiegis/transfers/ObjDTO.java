791fdd1f1e46b581ca34b02dbe1f9852d689d245
Nick Amelichev <nickamelichev@gmail.com>
2014-07-18 18:15:44 +0400

    timestamp, operation

--- remote-api/src/main/java/ru/zombator/zombiegis/transfers/ObjDTO.java
+++ remote-api/src/main/java/ru/zombator/zombiegis/transfers/ObjDTO.java
@@ -16,4 +16,30 @@ public final class ObjDTO implements Serializable {
      * Значения свойств объекта карты.
      */
     public ValueDTO[] values;
+    /**
+     * Штамп времени.
+     */
+    public Long timestamp;
+    /**
+     * Операция, информация о которой передана в DTO.
+     */
+    public Operation operation;
+
+    /**
+     * Операция над объектом.
+     */
+    public static enum Operation {
+        /**
+         * Объект был создан. В массиве <code>values</code> переданы значения всех свойств объекта.
+         */
+        CREATE,
+        /**
+         * Объект был изменен. В массиве <code>values</code> переданы все добавленные и измененные значения свойств объекта.
+         */
+        UPDATE,
+        /**
+         * Объект был удален. Массив <code>values</code> пуст.
+         */
+        DELETE
+    }
 }
