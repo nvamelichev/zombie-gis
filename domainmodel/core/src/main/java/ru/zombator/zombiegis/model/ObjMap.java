548373b32262c062ed2a7682d2607aed8ffcbdf5
Nick Amelichev <nickamelichev@gmail.com>
2014-07-21 04:00:02 +0400

    add javadoc, add ObjMap.isClosed()

--- domainmodel/core/src/main/java/ru/zombator/zombiegis/model/ObjMap.java
+++ domainmodel/core/src/main/java/ru/zombator/zombiegis/model/ObjMap.java
@@ -12,6 +12,21 @@ import ru.zombator.zombiegis.model.obj.event.ObjListener;
 
 /**
  * Интерфейс карты.
+ * <p>
+ * Карта может находиться в трех состояниях:
+ * <ul>
+ * <li>
+ * <em>не открыта</em> (<code>!{@link #isOpen() isOpen()} && !{@link #isClosed() isClosed()}</code>):
+ * можно делать что угодно, кроме {@link #objs(Predicate) получения объектов карты};
+ * </li>
+ * <li>
+ * <em>открыта</em> (<code>{@link #isOpen() isOpen()} && !{@link #isClosed() isClosed()}</code>):
+ * можно делать что угодно;
+ * </li>
+ * <li>
+ * <em>закрыта</em> (<code>!{@link #isOpen() isOpen()} && {@link #isClosed() isClosed()}</code>):
+ * можно вызвать только {@link #isClosed() isClosed()}, {@link #isOpen() isOpen()} и {@link #close() close()}.
+ * </ul>
  *
  * @author anv
  */
@@ -20,6 +35,8 @@ public interface ObjMap extends AutoCloseable {
      * Возвращает идентификатор карты.
      *
      * @return уникальный идентификатор карты
+     *
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      */
     long getId();
 
@@ -27,13 +44,22 @@ public interface ObjMap extends AutoCloseable {
      * Возвращает метаданные карты.
      *
      * @return метаданные карты
+     *
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      */
     @NonNull MapMeta getMeta();
 
     /**
      * Возвращает "мгновенный снимок" текущего состояния карты.
+     * <p>
+     * Является краткой формой вызова
+     * <code>
+     * {@link #objs(Predicate) objs}({@link ru.zombator.zombiegis.model.obj.util.Objs#all() all()}).
+     * </code>
      *
      * @return все объекты карты на данный момент
+     *
+     * @throws IllegalStateException <code>!{@link #isOpen() isOpen()}</code>
      */
     @NonNull ImmutableSet<Obj<?, ?>> objs();
 
@@ -46,16 +72,20 @@ public interface ObjMap extends AutoCloseable {
      * @return все объекты карты, удовлетворяющие условию в данный момент
      *
      * @see ru.zombator.zombiegis.model.obj.util.Objs Objs
+     *
+     * @throws IllegalStateException <code>!{@link #isOpen() isOpen()}</code>
      */
     @NonNull ImmutableSet<Obj<?, ?>> objs(@NonNull Predicate<Obj<?, ?>> pred);
 
     /**
      * Открывает карту - начинает загрузку объектов карты и подписывается на события
-     * изменения карты.
+     * изменения карты. Если карта уже открыта, ничего не делает.
      *
      * @throws GISException не удалось начать загрузку объектов карты
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      *
      * @see #isOpen()
+     * @see #isClosed()
      */
     void open() throws GISException;
 
@@ -65,25 +95,43 @@ public interface ObjMap extends AutoCloseable {
      * @return <code>true</code>, если карта открыта; иначе <code>false</code>
      *
      * @see #open()
+     * @see #isClosed()
      */
     boolean isOpen();
 
     /**
-     * Закрывает карту и отсоединяет от нее всех слушателей.
+     * Закрывает карту и отсоединяет от нее всех слушателей. Если карта уже закрыта, ничего не делает.
+     *
+     * @see #isClosed()
+     * @see #isOpen()
      */
     @Override
     void close();
 
     /**
+     * Проверяет, закрыта ли карта. Любые операции над закрытой картой вызывают {@link IllegalStateException},
+     * кроме {@link #isClosed()}, {@link #isOpen()} и {@link #close()}.
+     *
+     * @return <code>true</code>, если карта закрыта; иначе <code>false</code>
+     *
+     * @see #close()
+     */
+    boolean isClosed();
+
+    /**
      * Добавляет слушателя событий карты (открыта/закрыта).
      *
      * @param listener добавляемый слушатель
+     *
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      */
     void addListener(MapListener listener);
     /**
      * Удаляет слушателя событий карты (открыта/закрыта).
      *
      * @param listener удаляемый слушатель
+     *
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      */
     void removeListener(MapListener listener);
 
@@ -91,12 +139,16 @@ public interface ObjMap extends AutoCloseable {
      * Добавляет слушателя изменений объектов карты. Если карта {@link #isOpen()}
      *
      * @param listener добавляемый слушатель
+     *
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      */
     void addObjListener(ObjListener listener);
     /**
      * Удаляет слушателя изменений объектов карты.
      *
      * @param listener удаляемый слушатель
+     *
+     * @throws IllegalStateException {@link #isClosed() isClosed()}
      */
     void removeObjListener(ObjListener listener);
 }
