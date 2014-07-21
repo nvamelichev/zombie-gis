e7e4f5a23440364cf8200d31d96e79ccbd452451
Nick Amelichev <nickamelichev@gmail.com>
2014-07-21 04:18:06 +0400

    simplify

--- gis-api/src/main/java/ru/zombator/zombiegis/datasource/spi/def/BasicObjCreator.java
+++ gis-api/src/main/java/ru/zombator/zombiegis/datasource/spi/def/BasicObjCreator.java
@@ -1,9 +1,7 @@
 package ru.zombator.zombiegis.datasource.spi.def;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableSet;
-import java.util.Set;
 import org.netbeans.api.annotations.common.NonNull;
+
 import ru.zombator.zombiegis.datasource.ObjData;
 import ru.zombator.zombiegis.datasource.TypedProp;
 import ru.zombator.zombiegis.datasource.def.DefObj;
@@ -11,13 +9,11 @@ import ru.zombator.zombiegis.datasource.spi.ObjCreator;
 import ru.zombator.zombiegis.model.obj.DataModel;
 import ru.zombator.zombiegis.model.obj.Obj;
 import ru.zombator.zombiegis.model.obj.ViewModel;
-import ru.zombator.zombiegis.properties.Type;
 
 /**
  * Абстрактный базовый класс для создателей объектов предметной области.
  * <p>
- * Классам-наследникам достаточно указать набор поддерживаемых ими типов объектов и
- * реализовать методы создания {@link #createDataModel(ObjData) информационной модели} и
+ * Классам-наследникам достаточно реализовать методы создания {@link #createDataModel(ObjData) информационной модели} и
  * {@link #createViewModel(ObjData) модели отображения} объекта.
  *
  * @author nvamelichev
@@ -26,43 +22,13 @@ import ru.zombator.zombiegis.properties.Type;
  * @see #createViewModel(ObjData)
  */
 public abstract class BasicObjCreator implements ObjCreator {
-    private final Set<Type> types;
-
-    /**
-     * @param supported поддерживаемый тип объектов
-     */
-    protected BasicObjCreator(Type supported) {
-        this(new Type[]{supported});
-    }
-
-    /**
-     * @param supported поддерживаемые типы объектов
-     *
-     * @throws IllegalArgumentException <code>supported.length == 0</code>
-     */
-    protected BasicObjCreator(Type[] supported) {
-        Preconditions.checkArgument(supported.length > 0, "number of supported types must be >= 1");
-
-        this.types = ImmutableSet.copyOf(supported);
-    }
-
     @Override
     public boolean canConvert(ObjData data) {
         if (!data.getProps().hasValue(TypedProp.NAME)) {
             return false;
         }
 
-        Type type = data.getProps().get(TypedProp.TYPE);
-        if (type == null) {
-            return false;
-        }
-
-        for (Type supported: types) {
-            if (type.isSubtypeOf(supported)) {
-                return true;
-            }
-        }
-        return false;
+        return true;
     }
 
     @Override
